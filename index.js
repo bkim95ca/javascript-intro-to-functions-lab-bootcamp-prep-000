18783807fe85edb6e4516ad0c5a84b1332da63fb
Brandon Kim <bkim95ca@gmail.com>
2018-02-07 00:49:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) { 
   return "I can't hear you!"
  }
+ if (string.toUpperCase() === string) {
+   return "YES INDEED!"
+ }
 }
