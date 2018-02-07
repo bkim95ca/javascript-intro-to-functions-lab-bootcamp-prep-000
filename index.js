291b6ef405ca41a1c1d7cd003091b3d6147a6a7e
Brandon Kim <bkim95ca@gmail.com>
2018-02-07 00:48:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string){
+  if (string.toLowerCase() === string) { 
   return "I can't hear you!"
  }
 }
